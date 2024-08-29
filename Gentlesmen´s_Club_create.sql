170f3ad547730910e9414aff544951272b5d1583
EmersonPenelli <emersonpenelli@gmail.com>
2024-08-28 22:16:37 -0300

    sql modelagem de dados

--- Gentlesmen´s_Club_create.sql
+++ Gentlesmen´s_Club_create.sql
@@ -1,5 +1,5 @@
 -- Created by Vertabelo (http://vertabelo.com)
--- Last modification date: 2024-08-27 23:42:48.802
+-- Last modification date: 2024-08-29 01:05:06.791
 
 -- tables
 -- Table: AGENDAMENTO
@@ -19,19 +19,16 @@ CREATE TABLE AVALIACAO_BARBEIRO (
     nota int  NOT NULL,
     comentario text  NOT NULL,
     data_avaliacao timestamp  NOT NULL,
-    id_barbeiro int  NOT NULL,
-    id_usuario int  NOT NULL,
+    id_agendamento int  NOT NULL,
     CONSTRAINT AVALIACAO_BARBEIRO_pk PRIMARY KEY (id_avaliacao_barbeiro)
 );
 
 -- Table: BARBEARIA
 CREATE TABLE BARBEARIA (
     id_barbearia serial  NOT NULL,
-    nome_barbearia varchar(100)  NOT NULL,
-    horario_funcionamento varchar(255)  NOT NULL,
-    imagem varchar(255)  NOT NULL,
-    tel_whatsapp varchar(15)  NOT NULL,
-    id_cliente_parceiro int  NOT NULL,
+    cnpj varchar(25)  NOT NULL,
+    razao_social varchar(150)  NOT NULL,
+    id_cliente int  NOT NULL,
     CONSTRAINT BARBEARIA_pk PRIMARY KEY (id_barbearia)
 );
 
@@ -39,7 +36,7 @@ CREATE TABLE BARBEARIA (
 CREATE TABLE CLIENTE_PARCEIRO (
     id_cliente serial  NOT NULL,
     nota_privada int  NOT NULL,
-    id_usuario int  NOT NULL,
+    id_usuario int  NULL,
     CONSTRAINT CLIENTE_PARCEIRO_pk PRIMARY KEY (id_cliente)
 );
 
@@ -50,7 +47,7 @@ CREATE TABLE CONVENIENCIA (
     estacionamento boolean  NOT NULL,
     video_game boolean  NOT NULL,
     bar boolean  NOT NULL,
-    id_barbearia int  NOT NULL,
+    id_filial int  NOT NULL,
     CONSTRAINT CONVENIENCIA_pk PRIMARY KEY (id_conveniencia)
 );
 
@@ -58,24 +55,24 @@ CREATE TABLE CONVENIENCIA (
 CREATE TABLE ENDERECO (
     id_endereco serial  NOT NULL,
     logradouro varchar(255)  NOT NULL,
-    numero varchar(255)  NOT NULL,
+    numero varchar(50)  NOT NULL,
     complemento varchar(255)  NOT NULL,
     bairro varchar(255)  NOT NULL,
     cep varchar(15)  NOT NULL,
     cidade varchar(255)  NOT NULL,
     estado varchar(255)  NOT NULL,
     localizacao varchar(255)  NOT NULL,
-    id_barbearia int  NOT NULL,
+    id_filial int  NOT NULL,
     CONSTRAINT ENDERECO_pk PRIMARY KEY (id_endereco)
 );
 
--- Table: FRANQUIA
-CREATE TABLE FRANQUIA (
-    id_franquia serial  NOT NULL,
+-- Table: FILIAL
+CREATE TABLE FILIAL (
+    id_filial serial  NOT NULL,
     nome varchar(255)  NOT NULL,
     status boolean  NOT NULL,
     id_barbearia int  NOT NULL,
-    CONSTRAINT FRANQUIA_pk PRIMARY KEY (id_franquia)
+    CONSTRAINT FILIAL_pk PRIMARY KEY (id_filial)
 );
 
 -- Table: FUNCIONARIO
@@ -85,17 +82,9 @@ CREATE TABLE FUNCIONARIO (
     especialidade varchar(255)  NOT NULL,
     foto varchar(255)  NOT NULL,
     data_criacao timestamp  NOT NULL,
-    id_barbearia int  NOT NULL,
+    id_filial int  NOT NULL,
     CONSTRAINT email UNIQUE (especialidade) NOT DEFERRABLE  INITIALLY IMMEDIATE,
     CONSTRAINT FUNCIONARIO_pk PRIMARY KEY (id_barbeiro)
-);
-
--- Table: HISTORICO
-CREATE TABLE HISTORICO (
-    id_historico serial  NOT NULL,
-    valor_gasto decimal(10,2)  NOT NULL,
-    id_usuario int  NOT NULL,
-    CONSTRAINT HISTORICO_pk PRIMARY KEY (id_historico)
 );
 
 -- Table: PROMOCAO
@@ -115,22 +104,22 @@ CREATE TABLE REDE_SOCIAl (
     instagram varchar(255)  NOT NULL,
     facebook varchar(255)  NOT NULL,
     linkedin varchar(255)  NOT NULL,
-    tik_tok int  NOT NULL,
+    tik_tok varchar(50)  NOT NULL,
     youtube varchar(255)  NOT NULL,
     site_barbearia varchar(255)  NOT NULL,
-    id_barbearia int  NOT NULL,
+    id_filial int  NOT NULL,
     CONSTRAINT REDE_SOCIAl_pk PRIMARY KEY (id_rede_social)
 );
 
 -- Table: SERVICO
 CREATE TABLE SERVICO (
     id_servico serial  NOT NULL,
-    id_barbearia int  NOT NULL,
     nome_servico varchar(255)  NOT NULL,
     descricao varchar(255)  NOT NULL,
     preco decimal(10,2)  NOT NULL,
     duracao_minutos int  NOT NULL,
     disponivel boolean  NOT NULL,
+    id_filial int  NOT NULL,
     CONSTRAINT SERVICO_pk PRIMARY KEY (id_servico)
 );
 
@@ -147,120 +136,104 @@ CREATE TABLE USUARIO (
 );
 
 -- foreign keys
--- Reference: AGENDAMENTO_FUNCIONARIO (table: AGENDAMENTO)
-ALTER TABLE AGENDAMENTO ADD CONSTRAINT AGENDAMENTO_FUNCIONARIO
+-- Reference: Copy_of_AGENDAMENTO_FUNCIONARIO (table: AGENDAMENTO)
+ALTER TABLE AGENDAMENTO ADD CONSTRAINT Copy_of_AGENDAMENTO_FUNCIONARIO
     FOREIGN KEY (id_barbeiro)
     REFERENCES FUNCIONARIO (id_barbeiro)  
     NOT DEFERRABLE 
     INITIALLY IMMEDIATE
 ;
 
--- Reference: AGENDAMENTO_SERVICO (table: AGENDAMENTO)
-ALTER TABLE AGENDAMENTO ADD CONSTRAINT AGENDAMENTO_SERVICO
+-- Reference: Copy_of_AGENDAMENTO_SERVICO (table: AGENDAMENTO)
+ALTER TABLE AGENDAMENTO ADD CONSTRAINT Copy_of_AGENDAMENTO_SERVICO
     FOREIGN KEY (id_servico)
     REFERENCES SERVICO (id_servico)  
     NOT DEFERRABLE 
     INITIALLY IMMEDIATE
 ;
 
--- Reference: AVALIACAO_BARBEIRO_FUNCIONARIO (table: AVALIACAO_BARBEIRO)
-ALTER TABLE AVALIACAO_BARBEIRO ADD CONSTRAINT AVALIACAO_BARBEIRO_FUNCIONARIO
-    FOREIGN KEY (id_barbeiro)
-    REFERENCES FUNCIONARIO (id_barbeiro)  
+-- Reference: Copy_of_AVALIACAO_BARBEIRO_Copy_of_AGENDAMENTO (table: AVALIACAO_BARBEIRO)
+ALTER TABLE AVALIACAO_BARBEIRO ADD CONSTRAINT Copy_of_AVALIACAO_BARBEIRO_Copy_of_AGENDAMENTO
+    FOREIGN KEY (id_agendamento)
+    REFERENCES AGENDAMENTO (id_agendamento)  
     NOT DEFERRABLE 
     INITIALLY IMMEDIATE
 ;
 
--- Reference: AVALIACAO_BARBEIRO_USUARIO (table: AVALIACAO_BARBEIRO)
-ALTER TABLE AVALIACAO_BARBEIRO ADD CONSTRAINT AVALIACAO_BARBEIRO_USUARIO
-    FOREIGN KEY (id_usuario)
-    REFERENCES USUARIO (id_usuario)  
-    NOT DEFERRABLE 
-    INITIALLY IMMEDIATE
-;
-
--- Reference: BARBEARIA_CLIENTE_PARCEIRO (table: BARBEARIA)
-ALTER TABLE BARBEARIA ADD CONSTRAINT BARBEARIA_CLIENTE_PARCEIRO
-    FOREIGN KEY (id_cliente_parceiro)
+-- Reference: Copy_of_BARBEARIA_Copy_of_CLIENTE_PARCEIRO (table: BARBEARIA)
+ALTER TABLE BARBEARIA ADD CONSTRAINT Copy_of_BARBEARIA_Copy_of_CLIENTE_PARCEIRO
+    FOREIGN KEY (id_cliente)
     REFERENCES CLIENTE_PARCEIRO (id_cliente)  
     NOT DEFERRABLE 
     INITIALLY IMMEDIATE
 ;
 
--- Reference: CLIENTE_PARCEIRO_USUARIO (table: CLIENTE_PARCEIRO)
-ALTER TABLE CLIENTE_PARCEIRO ADD CONSTRAINT CLIENTE_PARCEIRO_USUARIO
+-- Reference: Copy_of_CLIENTE_PARCEIRO_USUARIO (table: CLIENTE_PARCEIRO)
+ALTER TABLE CLIENTE_PARCEIRO ADD CONSTRAINT Copy_of_CLIENTE_PARCEIRO_USUARIO
     FOREIGN KEY (id_usuario)
     REFERENCES USUARIO (id_usuario)  
     NOT DEFERRABLE 
     INITIALLY IMMEDIATE
 ;
 
--- Reference: CONVENIENCIA_BARBEARIA (table: CONVENIENCIA)
-ALTER TABLE CONVENIENCIA ADD CONSTRAINT CONVENIENCIA_BARBEARIA
+-- Reference: Copy_of_CONVENIENCIA_Copy_of_FILIAL (table: CONVENIENCIA)
+ALTER TABLE CONVENIENCIA ADD CONSTRAINT Copy_of_CONVENIENCIA_Copy_of_FILIAL
+    FOREIGN KEY (id_filial)
+    REFERENCES FILIAL (id_filial)  
+    NOT DEFERRABLE 
+    INITIALLY IMMEDIATE
+;
+
+-- Reference: Copy_of_ENDERECO_Copy_of_FILIAL (table: ENDERECO)
+ALTER TABLE ENDERECO ADD CONSTRAINT Copy_of_ENDERECO_Copy_of_FILIAL
+    FOREIGN KEY (id_filial)
+    REFERENCES FILIAL (id_filial)  
+    NOT DEFERRABLE 
+    INITIALLY IMMEDIATE
+;
+
+-- Reference: Copy_of_FRANQUIA_BARBEARIA (table: FILIAL)
+ALTER TABLE FILIAL ADD CONSTRAINT Copy_of_FRANQUIA_BARBEARIA
     FOREIGN KEY (id_barbearia)
     REFERENCES BARBEARIA (id_barbearia)  
     NOT DEFERRABLE 
     INITIALLY IMMEDIATE
 ;
 
--- Reference: ENDERECO_BARBEARIA (table: ENDERECO)
-ALTER TABLE ENDERECO ADD CONSTRAINT ENDERECO_BARBEARIA
-    FOREIGN KEY (id_barbearia)
-    REFERENCES BARBEARIA (id_barbearia)  
+-- Reference: Copy_of_FUNCIONARIO_Copy_of_FILIAL (table: FUNCIONARIO)
+ALTER TABLE FUNCIONARIO ADD CONSTRAINT Copy_of_FUNCIONARIO_Copy_of_FILIAL
+    FOREIGN KEY (id_filial)
+    REFERENCES FILIAL (id_filial)  
     NOT DEFERRABLE 
     INITIALLY IMMEDIATE
 ;
 
--- Reference: FRANQUIA_BARBEARIA (table: FRANQUIA)
-ALTER TABLE FRANQUIA ADD CONSTRAINT FRANQUIA_BARBEARIA
-    FOREIGN KEY (id_barbearia)
-    REFERENCES BARBEARIA (id_barbearia)  
-    NOT DEFERRABLE 
-    INITIALLY IMMEDIATE
-;
-
--- Reference: FUNCIONARIO_BARBEARIA (table: FUNCIONARIO)
-ALTER TABLE FUNCIONARIO ADD CONSTRAINT FUNCIONARIO_BARBEARIA
-    FOREIGN KEY (id_barbearia)
-    REFERENCES BARBEARIA (id_barbearia)  
-    NOT DEFERRABLE 
-    INITIALLY IMMEDIATE
-;
-
--- Reference: HISTORICO_USUARIO (table: HISTORICO)
-ALTER TABLE HISTORICO ADD CONSTRAINT HISTORICO_USUARIO
-    FOREIGN KEY (id_usuario)
-    REFERENCES USUARIO (id_usuario)  
-    NOT DEFERRABLE 
-    INITIALLY IMMEDIATE
-;
-
--- Reference: PROMOCAO_SERVICO (table: PROMOCAO)
-ALTER TABLE PROMOCAO ADD CONSTRAINT PROMOCAO_SERVICO
+-- Reference: Copy_of_PROMOCAO_SERVICO (table: PROMOCAO)
+ALTER TABLE PROMOCAO ADD CONSTRAINT Copy_of_PROMOCAO_SERVICO
     FOREIGN KEY (id_servico)
     REFERENCES SERVICO (id_servico)  
     NOT DEFERRABLE 
     INITIALLY IMMEDIATE
 ;
 
--- Reference: REDES_SOCIAl_BARBEARIA (table: REDE_SOCIAl)
-ALTER TABLE REDE_SOCIAl ADD CONSTRAINT REDES_SOCIAl_BARBEARIA
-    FOREIGN KEY (id_barbearia)
-    REFERENCES BARBEARIA (id_barbearia)  
+-- Reference: Copy_of_REDE_SOCIAl_Copy_of_FILIAL (table: REDE_SOCIAl)
+ALTER TABLE REDE_SOCIAl ADD CONSTRAINT Copy_of_REDE_SOCIAl_Copy_of_FILIAL
+    FOREIGN KEY (id_filial)
+    REFERENCES FILIAL (id_filial)  
     NOT DEFERRABLE 
     INITIALLY IMMEDIATE
 ;
 
--- Reference: SERVICO_BARBEARIA (table: SERVICO)
-ALTER TABLE SERVICO ADD CONSTRAINT SERVICO_BARBEARIA
-    FOREIGN KEY (id_barbearia)
-    REFERENCES BARBEARIA (id_barbearia)  
+-- Reference: Copy_of_SERVICO_Copy_of_FILIAL (table: SERVICO)
+ALTER TABLE SERVICO ADD CONSTRAINT Copy_of_SERVICO_Copy_of_FILIAL
+    FOREIGN KEY (id_filial)
+    REFERENCES FILIAL (id_filial)  
     NOT DEFERRABLE 
     INITIALLY IMMEDIATE
 ;
 
--- Reference: client_purchase (table: AGENDAMENTO)
-ALTER TABLE AGENDAMENTO ADD CONSTRAINT client_purchase
+-- Reference: Copy_of_client_purchase (table: AGENDAMENTO)
+ALTER TABLE AGENDAMENTO ADD CONSTRAINT Copy_of_client_purchase
     FOREIGN KEY (id_usuario)
     REFERENCES USUARIO (id_usuario)  
     NOT DEFERRABLE 
