9a4dfecc07f1a95c533c1e3b0e17e4060322991f
Hele-S <silvahelenaldo@edu.unisinos.br>
2024-09-04 18:34:32 -0300

    Formulário

--- gentlemens-club-app/src/components/LoginScreen.jsx
+++ gentlemens-club-app/src/components/LoginScreen.jsx
@@ -5,6 +5,8 @@ import {
     Container,
     RightSide,
     ImageLabel,
+    LeftSide,
+    Forms,
 
 } from "../styled_components/loginScreen/LoginScreenStyle"
 const LoginScreen = () => {
@@ -17,6 +19,18 @@ const LoginScreen = () => {
                         <p>― Glentlemen’s Club</p>
                     </ImageLabel>
                 </RightSide>
+                <LeftSide>
+                    <Forms>
+                        <img src="\src\assets\footer_svg\miniLogo_icon.svg" className="mini_logo" alt="Imagem de '\src\assets\footer_svg'" />
+                        <img src="\src\assets\LoginScreen_images\Welcome_message.svg" alt="Mensagem de boas vindas" />
+                        <h1>Login</h1>
+                        <p>Insira os detalhes da sua conta</p>
+                        <div>
+                            <label htmlFor="">Nome de usuário</label>
+                            <input type="text" />
+                        </div>
+                    </Forms>
+                </LeftSide>
             </Container>
         </Background>
     </>)
--- gentlemens-club-app/src/styled_components/loginScreen/LoginScreenStyle.jsx
+++ gentlemens-club-app/src/styled_components/loginScreen/LoginScreenStyle.jsx
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const Background = styled.div`
   width: 100vw;
   height: 100vh;
-  background-image: url('/src/assets/LoginScreen_png/Login Cliente - Desktop.png');
+  background-image: url('/src/assets/LoginScreen_images/Login Cliente - Desktop.png');
   background-size: cover; 
   background-position: center;
   background-repeat: no-repeat;
@@ -25,7 +25,7 @@ export const RightSide = styled.div`
     right:0;
     width:50%;
     height:100%;
-    background-image: url('/src/assets/LoginScreen_png/Barber_pole_login.png');
+    background-image: url('/src/assets/LoginScreen_images/Barber_pole_login.png');
     background-size: cover; 
     background-position: center;
     background-repeat: no-repeat;
@@ -60,5 +60,63 @@ export const ImageLabel = styled.div`
         font-weight:500;
         font-size:1.125rem;
         letter-spacing:-.04rem;
+    }
+`
+
+export const LeftSide = styled.div`
+    position: absolute;
+    left:0;
+    width:50%;
+    height:100%;
+    border-radius:50px 0 0 50px;
+`
+
+export const Forms = styled.form`
+    position: relative;
+    display: flex;
+    align-items:top;
+    flex-direction: column;
+    justify-content: center;
+    margin:0 auto;
+    margin-top:2.875rem;
+    width:60%;
+    & img{
+        height:2.875rem;
+        width:17rem;
+    }
+    & img.mini_logo{ // A cor ta meio fraca
+        align-items:top;
+        position:absolute;
+        width:fit-content;
+        left:-4.375rem;
+        transform:translate(-50%);
+        top:0;
+        
+    }
+    & h1{ //O G dessa fonte está diferente da letra G do figma, qual fonte usar?
+        color: #D2D2D2;
+        font-size:3rem;
+        height:fit-content;
+        margin-top:1rem;
+    }
+    & p{
+        font-size:1rem;
+        font-weight:bold;
+        color:#9C9C9C;
+        margin-top:1rem;
+    }
+    & input{
+        background:none;
+        border: none;
+        border-bottom: 1px solid #777879;
+        height:2.25rem;
+    }
+    & label{
+        position:absolute;
+        pointer-events:none;
+        color:#777879;
+        font-weight:600;
+        height:2.25rem;
+        align-content:center;
     }
 `
