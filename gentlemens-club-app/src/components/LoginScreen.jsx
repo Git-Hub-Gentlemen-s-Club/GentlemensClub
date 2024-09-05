2ca757155045c401185bbad5278ab0b3c755c4b4
Hele-S <silvahelenaldo@edu.unisinos.br>
2024-09-05 18:30:25 -0300

    LeftSide 99%

--- gentlemens-club-app/src/components/LoginScreen.jsx
+++ gentlemens-club-app/src/components/LoginScreen.jsx
@@ -1,5 +1,6 @@
+/* eslint-disable react/no-unknown-property */
 /* eslint-disable react/no-unescaped-entities */
-// import { useState } from "react"
+import { useState } from "react"
 import {
     Background,
     Container,
@@ -7,18 +8,29 @@ import {
     ImageLabel,
     LeftSide,
     Forms,
+    OtherLinks,
+
 
 } from "../styled_components/loginScreen/LoginScreenStyle"
 const LoginScreen = () => {
+    // const [passwordFocused, setPasswordFocused] = useState(false)
+    const [inputStates, setInputStates] = useState({
+        'Name': 0,
+        'Password': 0
+    });
+
+    const handleChange = (e) => {
+        const { name, value } = e.target
+        setInputStates((prevStates) => ({
+            ...prevStates,
+            [name]: value.length
+        }));
+        console.log(inputStates)
+    };
+
     return (<>
         <Background>
             <Container>
-                <RightSide>
-                    <ImageLabel>
-                        <h1>"Tradição e elegância em cada corte"</h1>
-                        <p>― Glentlemen’s Club</p>
-                    </ImageLabel>
-                </RightSide>
                 <LeftSide>
                     <Forms>
                         <img src="\src\assets\footer_svg\miniLogo_icon.svg" className="mini_logo" alt="Imagem de '\src\assets\footer_svg'" />
@@ -26,11 +38,51 @@ const LoginScreen = () => {
                         <h1>Login</h1>
                         <p>Insira os detalhes da sua conta</p>
                         <div>
-                            <label htmlFor="">Nome de usuário</label>
-                            <input type="text" />
+                            <input
+                                show={inputStates.Name}
+                                id="nameInput"
+                                name='Name'
+                                type="text"
+                                onChange={() => { handleChange }}
+                                placeholder="Nome de usuário"
+                            />
+                            <input
+                                show={inputStates.Name}
+                                id="passwordInput"
+                                name="Password"
+                                type="text"
+                                onChange={() => { handleChange }}
+                                placeholder="Senha"
+                            />
+                            <img className="showPassword" src="\src\assets\LoginScreen_images\Show_password.svg" alt="" />
+                        </div>
+                        <div>
+                            <a href="#">Esqueceu sua senha?</a>
+                            <button>Login</button>
                         </div>
                     </Forms>
+                    <OtherLinks>
+                        <div>
+                            <button>
+                                <img src="\src\assets\LoginScreen_images\Google_icon.png" alt="" />
+                                <p>Entrar com Google</p>
+                            </button>
+                            <button>
+                                <img src="\src\assets\LoginScreen_images\Facebook_icon.png" alt="" />
+                                <p>Entrar com Facebook</p></button>
+                        </div>
+                        <div className="BottomDiv">
+                            <p className="BottomText" >Não tem uma conta?</p>
+                            <button className="SignIn">Inscreva-se</button>
+                        </div>
+                    </OtherLinks>
                 </LeftSide>
+                <RightSide>
+                    <ImageLabel>
+                        <h1>"Tradição e elegância em cada corte"</h1>
+                        <p>― Glentlemen’s Club</p>
+                    </ImageLabel>
+                </RightSide>
             </Container>
         </Background>
     </>)
--- gentlemens-club-app/src/styled_components/loginScreen/LoginScreenStyle.jsx
+++ gentlemens-club-app/src/styled_components/loginScreen/LoginScreenStyle.jsx
@@ -2,35 +2,58 @@ import styled from "styled-components";
 
 export const Background = styled.div`
   width: 100vw;
+  min-height:fit-content;
   height: 100vh;
   background-image: url('/src/assets/LoginScreen_images/Login Cliente - Desktop.png');
   background-size: cover; 
   background-position: center;
   background-repeat: no-repeat;
   color:#101010;
+  overflow-y:auto;
+  & button{
+    &:hover{
+        cursor:pointer;
+    }
+  }
 `
 
 export const Container = styled.div`
+    display:flex;
+    justify-content:center;
+    flex-direction:row;
     position:relative;
     margin:0 auto;
-    top:5vh;
-    width:85vw;
-    height:90vh;
+    margin-top:5vh;
+    margin-bottom:5vh;
+    max-width:85vw;
     background-color:#1C1D20;
     border-radius:50px;
+    @media (max-width: 810px) {
+        position:relative;
+        flex-direction:column;
+        height:fit-content;
+        width:100vw;
+        margin:0;
+        max-width:none;
+        border-radius:0;
+    }
 `
 
 export const RightSide = styled.div`
-    position: absolute;
-    right:0;
-    width:50%;
+    position: relative;
+    width:100%;
+    min-width:50%;
     height:100%;
+    min-height:50rem;
     background-image: url('/src/assets/LoginScreen_images/Barber_pole_login.png');
-    background-size: cover; 
+    background-size: 100% 100%; 
     background-position: center;
     background-repeat: no-repeat;
     border-radius: 0 50px 50px 0;
-    /* background-color:blue; */
+    @media (max-width: 810px) {
+        /* display:none; */
+        border-radius:0;
+    }
 `
 
 export const ImageLabel = styled.div`
@@ -42,7 +65,7 @@ export const ImageLabel = styled.div`
     width:100%;
     height:12%;
     background-color:#1C1D21;
-    color:#d48e00;
+    color:#B58934;
     text-align:center;
     & h1 {
         font-family:'Playfair Display', serif;
@@ -64,11 +87,19 @@ export const ImageLabel = styled.div`
 `
 
 export const LeftSide = styled.div`
-    position: absolute;
-    left:0;
-    width:50%;
+    display:flex;
+    flex-direction:column;
+    position: relative;
+    width:100%;
+    min-width:50;
     height:100%;
+    min-height:fit-content;
     border-radius:50px 0 0 50px;
+    margin:0 auto;
+    margin-top:2.875rem;
+    @media (max-width: 810px) {
+        border-radius:0;
+    }
 `
 
 export const Forms = styled.form`
@@ -76,19 +107,28 @@ export const Forms = styled.form`
     display: flex;
     align-items:top;
     flex-direction: column;
-    justify-content: center;
+    min-height:fit-content;
     margin:0 auto;
-    margin-top:2.875rem;
+    
     width:60%;
+    overflow:visible;
+
     & img{
-        height:2.875rem;
-        width:17rem;
-    }
+    display: inline-block;
+    vertical-align: top;
+    height: 36px; 
+    object-fit:none;
+    width: 0;
+    min-width: 17rem; 
+    /* min-width: fit-content;  */
+  }
+
     & img.mini_logo{ // A cor ta meio fraca
+        all:unset;
         align-items:top;
         position:absolute;
         width:fit-content;
-        left:-4.375rem;
+        left:-15%;
         transform:translate(-50%);
         top:0;
         
@@ -110,6 +150,13 @@ export const Forms = styled.form`
         border: none;
         border-bottom: 1px solid #777879;
         height:2.25rem;
+        color: #777879;
+        font-size:1rem;
+        font-weight:600;
+        width:100%;
+        &::placeholder{
+        font-weight:600;
+        }   
     }
     & label{
         position:absolute;
@@ -119,4 +166,105 @@ export const Forms = styled.form`
         height:2.25rem;
         align-content:center;
     }
+    & div{
+        display:flex;
+        /* width:fit */
+        justify-content:center;
+        position:relative;
+        margin-top:2rem;
+        flex-direction:column;
+        gap:2rem;
+    }
+    & .showPassword{
+        all:unset;
+        position:absolute;
+        bottom:0;
+        right:0;
+        transform:translate(-50%, -50%);
+        width:1.5rem;
+    }
+    & a{
+        text-decoration:none;
+        color:#777879;
+        font-weight:600;
+        &:hover{
+            text-decoration:underline;
+        }
+    }
+    & button{
+        width:100%;
+        max-width:40rem;
+        background-color: #B58934;
+        color:#D2D2D2;
+        border:none;
+        border-radius:12px;
+        padding: 1rem 0;
+        font-size:1.2rem;
+        margin:0 auto;
+    }
 `
+
+export const OtherLinks = styled.div`
+    display:flex;
+    margin:0 auto;
+    position:relative;
+    width:60%;
+    margin-top:4rem;
+    gap:4rem;
+    flex-direction:column;
+    & div{
+        display:flex;
+        flex-direction:column;
+        gap:1rem;
+        width:100%;
+    }
+    & button{
+        /* text-align:center; */
+        display:flex;
+        justify-content:center;
+        align-content:left;
+        width:100%;
+        max-width:40rem;
+        max-height:55px;
+        margin:0 auto;
+        background-color: #D2D2D2;
+        color:#000;
+        border:none;
+        border-radius:12px;
+        padding: 1rem 0;
+        font-size:1.2rem;
+
+        & img{
+            position:relative;
+            margin-right:8px;
+            transform:translateY(-15%)
+        }
+        & p {
+            min-width:200px;
+            max-width:100px;
+            display: flex;
+            justify-content: center;
+            margin: 0;
+        }
+    }
+    & p.BottomText{
+        font-size:1rem;
+        font-weight:400;
+        align-content:center;
+        color:#9C9C9C;
+        } 
+    & div.BottomDiv{
+        flex-direction:row;
+        margin-bottom:8rem;
+        & button{
+            background-color:#2F3032;
+            color:#9C9C9C;
+            width:auto;
+            padding:1rem;
+
+        }
+    }
+        
+    
+`
+
