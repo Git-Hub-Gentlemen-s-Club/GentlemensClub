5e55349687d64d4ea6f0103db2c48b63a848b0db
SamuelRicardoo <sricardosgomes@hotmail.com>
2024-11-07 18:56:37 -0300

    Deletando api com bug

--- GentlemensClub/src/main/java/repository/UserVerifierRepositoryEmail.java
+++
@@ -1,8 +0,0 @@
-package repository;
-
-import entities.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserVerifierRepositoryEmail extends JpaRepository<User, Long> {
-    boolean existsByEmail (String email);
-}
